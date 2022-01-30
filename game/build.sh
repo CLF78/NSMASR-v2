f0dda9a83d1e88b28dab0255b721979ad21fd8f4
CLF78 <twomp021@gmail.com>
2022-01-30 15:43:53 +0100

    Move compiler output to separate dirs
    Bash only!

--- game/build.sh
+++ game/build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -d 'build' ]; then
+	rm -rf 'build'
+fi
+mkdir -p 'build'
+
 AS="../cw/mwasmeppc.exe"
 ASMFLAGS='-I- -i ../include'
 ASMFILES=''
@@ -8,29 +13,32 @@ CC="../cw/mwcceppc.exe"
 CFLAGS='-I- -i ../include -nostdinc -Cpp_exceptions off -enum int -O4 -use_lmw_stmw on -fp hard -rostr -sdata 0 -sdata2 0'
 CPPFILES='1-nsmbw-osreport'
 
+MYREGION='P1'
+
 OBJECTS=''
 
 for i in $CPPFILES; do
     echo "Compiling $i.cpp..."
-    wine $CC $CFLAGS -c -o $i.o $i.cpp
+    wine $CC $CFLAGS -c -o build/$i.o $i.cpp
     if [ $? != 0 ]; then
         exit 1
     else
-        OBJECTS="$OBJECTS $i.o"
+        OBJECTS="$OBJECTS build/$i.o"
     fi
 done
 
 for i in $ASMFILES; do
     echo "Assembling $i.S..."
-    wine $AS $ASMFLAGS -c -o $i.o $i.S
+    wine $AS $ASMFLAGS -c -o build/$i.o $i.S
     if [ $? != 0 ]; then
         exit 1
     else
-        OBJECTS="$OBJECTS $i.o"
+        OBJECTS="$OBJECTS build/$i.o"
     fi
 done
 
+echo 'Linking...'
+Kamek $OBJECTS -dynamic -externals=externals-nsmbw.txt -versions=versions-nsmbw.txt -output-kamek=build/code\$KV\$.bin
 if [ $? == 0 ]; then
-    echo 'Linking...'
-    Kamek $OBJECTS -dynamic -externals=externals-nsmbw.txt -versions=versions-nsmbw.txt -output-kamek=code\$KV\$.bin
+    mv build/code$MYREGION.bin ~/.local/share/dolphin-emu/Load/Riivolution/nsmb
 fi
--- loader/build.sh
+++ loader/build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+if [ -d 'build' ]; then
+	rm -rf 'build'
+fi
+mkdir -p 'build'
+
 CC="../cw/mwcceppc.exe"
 CPPFILES='kamekLoader nsmbw'
 CFLAGS='-i . -I- -i ../include -nostdinc -Cpp_exceptions off -enum int -O4 -use_lmw_stmw on -fp hard -rostr -sdata 0 -sdata2 0'
 
 for i in $CPPFILES; do
     echo Compiling $i.cpp...
-    wine $CC $CFLAGS -c -o $i.o $i.cpp
+    wine $CC $CFLAGS -c -o build/$i.o $i.cpp
     if [ $? != 0 ]; then
         exit 1
     fi
 done
 
-if [ $? == 0 ]; then
-    echo 'Linking...'
-    Kamek kamekLoader.o nsmbw.o -externals=addresses.txt -static=0x800046E4 -output-code=loader.bin -output-riiv=loader.xml
-fi
+echo 'Linking...'
+Kamek build/kamekLoader.o build/nsmbw.o -externals=externals-nsmbw.txt -static=0x800046E4 -output-code=build/loader.bin -output-riiv=build/loader.xml
