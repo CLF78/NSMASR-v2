4a5f4ae6e7eb3b2428f1df01a4c7e488216faa21
CLF78 <twomp021@gmail.com>
2022-02-10 09:53:59 +0100

    Fix some fatal bugs

--- game/animtiles.cpp
+++ game/animtiles.cpp
@@ -10,15 +10,15 @@ void ProcessAnimsBin(dBgTexMng_c* mng, AnimsBin* animMeta, int slot, char* tileN
     char buffer[32];
 
     for (int i = 0; i < animMeta->numEntries; i++) {
-        // Print name to buffer
-        snprintf(buffer, sizeof(buffer), ANIMTILE, i);
-
         // Get relevant anim entry and framesheet
         AnimsBinEntry* currAnim = &animMeta->entries[i];
         u8* currAnimDelays = (u8*)((u32)animMeta + currAnim->delayOffs);
 
+        // Print name to buffer
+        snprintf(buffer, sizeof(buffer), ANIMTILE, currAnim->tileNum);
+
         // Setup the animated tile (function takes care of everything)
-        mng->setupAnimTile(slot, currAnim->tileNum, buffer, currAnimDelays, currAnim->reverse);
+        mng->setupAnimTile(slot, 0x100 * slot | currAnim->tileNum, buffer, currAnimDelays, currAnim->reverse);
     }
 }
 
@@ -39,15 +39,15 @@ kmCallDefCpp(0x80087840, void, dBgTexMng_c* mng, int slot, u16 tileNum, char* na
         // Get the tileset name
         char* tileName = dBgGlobal_c::instance->getEnvironment(area, slot);
 
-        // If tileset is null, skip
-        if (!tileName || tileName[0] == '\0')
+        // If tileset isn't set, skip
+        if (tileName[0] == '\0')
             continue;
 
         // Get anims.bin from relevant file
         AnimsBin* animMeta = (AnimsBin*)dResMng_c::instance->res.getRes(tileName, ANIMMETA);
 
         // If file was not found or the version mismatches, skip
-        if (!animMeta || animMeta->version != SPECVERSION)
+        if (animMeta == NULL || animMeta->version != SPECVERSION)
             continue;
 
         // Process anims file
