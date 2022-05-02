6a324685eb00afab64408e189f624d261c13f5fd
CLF78 <twomp021@gmail.com>
2022-05-02 15:01:28 +0200

    Dehardcode 1-1 background

---
+++ game/background.cpp
@@ -0,0 +1,61 @@
+#include <kamek.h>
+#include <course/BgData.h>
+#include <fBase/dBase/dBaseActor/dActor/daFarBG.h>
+
+extern "C" {
+u32 GetFixedBGEnabled(daFarBG_c* farbg);
+u32 GetFixedBG(daFarBG_c* farbg);
+}
+
+u32 GetFixedBG(daFarBG_c* farbg) {
+
+    // Get background data
+    BgData* bgData = farbg->GetMyBgDataP();
+
+    // If the fixed bg id isn't zero use it, else return the hardcoded value
+    return (bgData->fixedBgFileId) ? bgData->fixedBgFileId : 0x104;
+}
+
+u32 GetFixedBGEnabled(daFarBG_c* farbg) {
+
+    // Get background data
+    BgData* bgData = farbg->GetMyBgDataP();
+
+    // If the fixed bg id is zero return 3, else 1
+    return (bgData->fixedBgFileId == 0) * 2 + 1;
+}
+
+// Disable original check, remove if you want to keep it!
+kmWrite32(0x801180A0, 0x38600001);
+
+// Check if enabled
+kmBranchDefAsm(0x801180AC, 0x801180B0) {
+    // No stack saving needed
+    nofralloc
+
+    // Call CPP function
+    mr r3, r24
+    bl GetFixedBGEnabled
+
+    // Move result to r0 and return
+    mr r0, r3
+    blr
+}
+
+// Apply modified value
+kmBranchDefAsm(0x8011810C, 0x80118110) {
+    // No stack saving needed
+    nofralloc
+
+    // Call CPP function
+    mr r3, r24
+    bl GetFixedBG
+
+    // Move result to r4
+    mr r4, r3
+
+    // Restore registers and return
+    mr r3, r24
+    li r0, 0
+    blr
+}
